ac1eaf3a314dafe99b9dc20afad7525fa27db4b4
Scott Lieber <scottlieber@Scotts-MacBook-Air.local>
2026-01-14 15:28:13 -0500

    New Tweets

--- tweets/tweets_7_other/54_antizionistJews.js
+++ tweets/tweets_7_other/54_antizionistJews.js
@@ -21,12 +21,24 @@ const examples54 = [
     dateAdded: "",
     date: "2026-01-07",
     claimIds: ["54", "19"],
-    exampleId: "00005401",
+    exampleId: "00005402",
     keywordIds: [],
     text: "As Allied troops approached the death camps the Nazis prioritized killing the kapos rather than the other Jews. Because everyone despises traitors.",
     standaloneTweets: ["2009092163133678074"],
     themTweets: [],
     usTweets: []
+  },
+  {
+    dateAdded: "",
+    date: "2026-01-09",
+    claimIds: ["54"],
+    exampleId: "00005403",
+    keywordIds: [],
+    text: "Brad Lander, a Jewish NYC official, endorses Zohran Mamdani, a known antizionist. But when he condemns Hamas, he's no longer accepted. Because you can never be pure enough.",
+    standaloneTweets: [],
+    thenVsNowFormat: true,
+    thenTweets: ["1933550111298088971", "1937715119326138382", "1982181696335175966"],
+    nowTweets: ["2009658373231534414", "2009679109689926055", "2009766472592118122", "2009680818130825583", "2009740574538998200", "2009718212557189464", "2009806295403245783", "2009676070724010432", "2009886414075564400", "2009765761837842602", "2009778761168728314", "2009828470793089390"]
   }
 ]
 
