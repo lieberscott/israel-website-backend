3d77de180eab3ff92a8baabe47e0f2b3711f7bd5
Scott Lieber <scottlieber@Scotts-MacBook-Air.local>
2026-02-11 14:25:58 -0500

    New Tweets

--- tweets/tweets_7_other/32_mediaBias.js
+++ tweets/tweets_7_other/32_mediaBias.js
@@ -94,6 +94,16 @@ const examples32 = [
     themTweets: ["1714343713801957435", "1714353899040358550", "1714619599017157072", "1714336328312144180", "1714593437142393153"],
     usTweets: ["1714708365899612274", "1714552335051989483", "1714570324455416202", "1714552321760202859", "1714552323945472323","1714552325988053127", "1714552328471138777" , "1714552330605973966", "1714552332749357479", "1714579782761959791", "1714588649361207627"]
   },
+  {
+    dateAdded: "",
+    date: "2026-12-11",
+    claimIds: ["32"],
+    exampleId: "000001313",
+    keywordIds: [],
+    text: "On Oct. 16, 2023, reports came out that Israel had bombed al-Alhi hospital, killing 500 people. The media ran with it. The NY Times, AP, Reuters, Guardian, and LA Times all had it prominantly in their publications. The LA Times ran a front-page headline: 'At least 500 killed in Israeli airstrike on Gaza City Hospital, Gaza Healthy Ministry says'. In reality, an errant Palestinian Islamic Jihad rocket hit the hospital parking lot, as demonstrated in subsequent days by video footage, intercepted calls between Palestinian groups, and multiple independent open-source intel analaysts. The hospital itself was fully in tact and operational. And there were less than 10 killed.",
+    themTweets: ["1714343713801957435", "1714353899040358550", "1714619599017157072", "1714336328312144180", "1714593437142393153"],
+    usTweets: ["2021303344090776005"]
+  },
 ]
 
 module.exports = { claim32, examples32 };
